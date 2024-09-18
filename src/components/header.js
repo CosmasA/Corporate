cc87567b56f140eade78b5c640e1dd45b9a22c4a
CosmasA <cosmasapuke@gmail.com>
2024-09-18 17:59:26 +0300

    A

--- src/components/header.js
+++ src/components/header.js
@@ -12,14 +12,14 @@ function AppHeader() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/corporate">Home</Nav.Link>
-            <Nav.Link href="/corporate/about">About</Nav.Link>
-            <Nav.Link href="/corporate/services">Services</Nav.Link>
-            <Nav.Link href="/corporate/works">Works</Nav.Link>
-            <Nav.Link href="/corporate/teams">Teams</Nav.Link>
-            <Nav.Link href="/corporate/testimonials">Testimonials</Nav.Link>
-            <Nav.Link href="/corporate/pricing">Pricing</Nav.Link>
-            <Nav.Link href="/corporate/blog">Blog</Nav.Link>
-            <Nav.Link href="/corporate/contact">Contact</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/services">Services</Nav.Link>
+            <Nav.Link href="/works">Works</Nav.Link>
+            <Nav.Link href="/teams">Teams</Nav.Link>
+            <Nav.Link href="/testimonials">Testimonials</Nav.Link>
+            <Nav.Link href="/pricing">Pricing</Nav.Link>
+            <Nav.Link href="/blog">Blog</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
