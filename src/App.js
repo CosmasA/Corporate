3f62b6333e141533cd2d79e50b5b92a7432892b4
CosmasA <cosmasapuke@gmail.com>
2024-09-18 13:43:08 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import AppHeader from "./components/header";
+import AppFooter from "./components/footer";
+import AppAbout from "./components/about";
+import AppHero from "./components/hero";
+import AppServices from "./components/services";
+import AppWorks from "./components/works";
+import AppTeams from "./components/teams";
+import AppTestimonials from "./components/testimonials";
+import AppPricing from "./components/pricing";
+import AppBlog from "./components/blog";
+import AppContact from "./components/contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header id="header">
+          <AppHeader />
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<AppHero />} />
+            <Route path="/about" element={<AppAbout />} />
+            <Route path="/services" element={<AppServices />} />
+            <Route path="/works" element={<AppWorks />} />
+            <Route path="/teams" element={<AppTeams />} />
+            <Route path="/testimonials" element={<AppTestimonials />} />
+            <Route path="/pricing" element={<AppPricing />} />
+            <Route path="/blog" element={<AppBlog />} />
+            <Route path="/contact" element={<AppContact />} />
+          </Routes>
+        </main>
+
+        <footer id="footer">
+          <AppFooter />
+        </footer>
+      </div>
+    </Router>
   );
 }
 
