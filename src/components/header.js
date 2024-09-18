ddeaf05553af066c3064914e86cd1543f70f7ea7
CosmasA <cosmasapuke@gmail.com>
2024-09-18 17:53:24 +0300

    A

--- src/components/header.js
+++ src/components/header.js
@@ -7,19 +7,19 @@ function AppHeader() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand to={"/corporate"}>Corporate</Navbar.Brand>
+        <Navbar.Brand href="/corporate">Corporate</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link to={"/corporate"}>Home</Nav.Link>
-            <Nav.Link to={"/corporate/about"}>About</Nav.Link>
-            <Nav.Link to={"/corporate/services"}>Services</Nav.Link>
-            <Nav.Link to={"/corporate/works"}>Works</Nav.Link>
-            <Nav.Link to={"/corporate/teams"}>Teams</Nav.Link>
-            <Nav.Link to={"/corporate/testimonials"}>Testimonials</Nav.Link>
-            <Nav.Link to={"/corporate/pricing"}>Pricing</Nav.Link>
-            <Nav.Link to={"/corporate/blog"}>Blog</Nav.Link>
-            <Nav.Link to={"/corporate/contact"}>Contact</Nav.Link>
+            <Nav.Link href="/corporate">Home</Nav.Link>
+            <Nav.Link href="/corporate/about">About</Nav.Link>
+            <Nav.Link href="/corporate/services">Services</Nav.Link>
+            <Nav.Link href="/corporate/works">Works</Nav.Link>
+            <Nav.Link href="/corporate/teams">Teams</Nav.Link>
+            <Nav.Link href="/corporate/testimonials">Testimonials</Nav.Link>
+            <Nav.Link href="/corporate/pricing">Pricing</Nav.Link>
+            <Nav.Link href="/corporate/blog">Blog</Nav.Link>
+            <Nav.Link href="/corporate/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
